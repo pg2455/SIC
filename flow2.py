2eaf8b4a5669905360c706db49018f4cbe28ea88
Prateek Gupta <pg2455@columbia.edu>
2016-02-20 14:45:38 -0500

    first commit

---
+++ flow2.py
@@ -0,0 +1,31 @@
+
+from flowthings import API, Token 
+from flask import Flask, request, jsonify
+import time 
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/getMS')
+def msData():
+	data = api.drop(ms6).find()
+	return jsonify({"data":data})
+
+
+if __name__ =="__main__":
+	token = 'l2VuxNAeUuNnQcncNSL8vUjckYcH7Kyu'
+	account = 'prateek91'
+	flow_path = '/stamford2016/hack-data/by-location/sic/wad/ms6'
+
+
+	ms6 = 'f56c4c83d68056d7a7d420452'
+	pg6 = 'f56c4c82b68056d7a7d420412'
+	ps6 = 'f56c4c82768056d7a7d420405'
+
+	creds = Token(account, token)
+	api  = API(creds)
+
+	app.debug =True
+	app.run('localhost', port =8080)
